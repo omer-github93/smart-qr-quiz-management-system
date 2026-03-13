0a4101a2501764fdb7545a33e9a633e65dcf52e6
omer <omer.hamajan@gmail.com>
2026-03-13 10:35:58 +0300

    Changing base url from frontend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://laravel-quiz-api-production.up.railway.app/',
     headers: {
         'Accept': 'application/json',
     }
