63d40783f63e11fc53b089049a4d64f33c66c3e7
omer <omer.hamajan@gmail.com>
2026-03-13 10:55:53 +0300

    Updated the URL for production

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -18,6 +18,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
